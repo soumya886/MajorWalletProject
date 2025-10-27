ea506a4c9ca850084ea5d34236aba30c30c56e46
Soumya Dhal <soumyadhal2000@gmail.com>
2025-10-27 12:26:12 +0530

    Made the changes in the wallet service user dto

--- 3-wallet-service/src/main/java/com/example/demo/dto/UserDto.java
+++ 3-wallet-service/src/main/java/com/example/demo/dto/UserDto.java
@@ -5,10 +5,7 @@ import lombok.Data;
 @Data
 public class UserDto {
 private int userId;
-private String userName;
-
-private String password;
-	
+private String userName;	
 private int phoneNo;
 private String address;
 private String email;
